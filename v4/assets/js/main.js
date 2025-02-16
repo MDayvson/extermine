82d70964c2258d9a998b38cda5c1687f117f3a42
MDayvson <mdayvson@outlook.com>
2025-02-16 13:31:05 -0300

    v4

--- v4/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-import { initScrollReveal } from "./scrollReveal.js";
-import { typeWrite } from "./typeWrite.js";
-import { menu } from "./menu.js";
-
-menu();
-initScrollReveal();
-typeWrite(document.querySelector(".typewriter"));
